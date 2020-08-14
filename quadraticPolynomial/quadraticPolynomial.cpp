9c0c2544a8dcc25e8edb1a8eb2fb73e827e2f298
VACHARA VIRIYAKUL <63010871@KMITL.AC.TH>
2020-08-14 16:19:05 +0700

    Add project files.

---
+++ quadraticPolynomial/quadraticPolynomial.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+
+int main()
+{
+    double a, b, c, x1, x2;
+    printf("input coefficient x^2,x,number Respectively : ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+    x1 = ((-1 * b) + (sqrt(pow(b, 2) - (4 * a * c)))) / (2 * a);
+    x2 = ((-1 * b) - (sqrt(pow(b, 2) - (4 * a * c)))) / (2 * a);
+    if (pow(b, 2) - (4 * a * c) == 0)
+    {
+        printf("x = %.2lf", x1);
+    }
+    else if (pow(b, 2) - (4 * a * c) > 0)
+    {
+        if (x1 > x2) {
+            printf("x1 = %.2lf, x2 = %.2lf", x2, x1);
+        }
+        else if (x1 < x2)
+        {
+            printf("x1 = %.2lf, x2 = %.2lf", x1, x2);
+        }
+
+    }
+    else if (pow(b, 2) - (4 * a * c) < 0)
+    {
+        printf("impossible to solve");
+    }
+
+    return 0;
+
+}
